655a6dc4945fdee0daa0f2422f3f188f8b6aa757
sarwarhusain <sarwarahmed693@.com>
2021-11-07 13:50:15 +0600

    myOrder change

--- index.js
+++ index.js
@@ -61,27 +61,32 @@ async function run() {
       const result = await servicesCollection.deleteOne(query);
       res.json(result);
     });
-    // DELETE API
-    app.delete('/services/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await servicesCollection.deleteOne(query);
-      res.json(result);
-    });
+
 
     // confirm order
-    app.post("/confirmOrder", async (req, res) => {
+    app.post('/confirmOrder', async (req, res) => {
       const result = await ordersCollection.insertOne(req.body);
       res.send(result);
     });
 
     // my confirmOrder
-    app.get("/myOrders/:email", async (req, res) => {
-      const result = await ordersCollection
+
+    app.get('/myOrders/:email', async (req, res) => {
+      const result = await bookingsCollection
         .find({ email: req.params.email })
         .toArray();
       res.send(result);
     });
+
+    /// delete order
+    app.delete('/deleteOrder/:id', async (req, res) => {
+      const result = await ordersCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    // 
   }
   finally {
     // await client.close();
